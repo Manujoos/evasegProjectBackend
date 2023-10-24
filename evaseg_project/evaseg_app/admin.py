96c446e9e8943382d9ebe56c5c424eb77bbb18d2
Manujoos <125741091+Manujoos@users.noreply.github.com>
2023-10-24 11:55:15 -0500

    Delete evaseg_project directory

--- evaseg_project/evaseg_app/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-
-from evaseg_app.models import Rol, TipoDocumento, Usuario, Login, Ficha, InstructorFicha, AprendizFicha, TipoProceso, DetalleProceso, Citacion
-admin.site.register([Rol, TipoDocumento, Usuario, Login, Ficha, InstructorFicha, AprendizFicha, TipoProceso, DetalleProceso, Citacion])
--- evaseg_project/evaseg_app/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class EvasegAppConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'evaseg_app'
